343d9192a2a1058478565c2ea78cf365f01633e2
Dulkith Bataduwa <dulkith@outlook.com>
2019-04-19 09:45:38 +0530

    Re-train  new data set

---
+++ guess.sh
@@ -0,0 +1,2 @@
+python scripts/label_image.py \
+--image sunflower-450231_960_720.jpeg
--- scripts/show_image.py
+++ scripts/show_image.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
 #
-# Copyright 2018 Thuru Care.
+# Copyright 2017 Google Inc.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -10,7 +21,7 @@ import os
 from IPython.display import Image, HTML, display
 
     
-root = "tf_files/flower_photos/"
+root = "/tf_files/thuru_care_data_set/"
 with open(root+"/LICENSE.txt") as f:
     attributions = f.readlines()[4:]
 attributions = [line.split(' CC-BY') for line in attributions]
